23e00934e08bd5b97e1ed6ad2e82f8ee41deb91c
tzh <2375626546@qq.com>
2018-11-13 13:06:05 +0800

    使用restful

--- app.py
+++ app.py
@@ -2,16 +2,21 @@
 
 from datetime import datetime
 
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
+from flask_restful import Api, Resource
 from flask_sockets import Sockets
 
 app = Flask(__name__)
+api = Api(app)
 sockets = Sockets(app)
 
 
-@app.route('/')
-def index():
-  return 'hello'
+class HelloWorld(Resource):
+  def get(self):
+    return {'hello': 'world'}
+
+
+api.add_resource(HelloWorld, '/')
 
 
 @app.route('/time')
