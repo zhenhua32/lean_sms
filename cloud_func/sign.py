12abd758ce7ea70385eae6e029fd7f2196ecbfd7
tzh <2375626546@qq.com>
2018-11-14 15:26:47 +0800

    修复bug, 有个变量名字写错了

--- cloud_func/sign.py
+++ cloud_func/sign.py
@@ -16,9 +16,9 @@ https://leancloud.cn/docs/realtime_v2.html#hash-188224612
 engine = Engine()
 
 
-def get_signature(text, timestamp, none):
+def get_signature(text, timestamp, nonce):
   signature = hmac.new(MASTER_KEY.encode('utf-8'), text.encode('utf-8'), hashlib.sha1).hexdigest()
-  data = {'signature': signature, 'timestamp': timestamp, 'none': none}
+  data = {'signature': signature, 'timestamp': timestamp, 'nonce': nonce}
   return data
 
 
@@ -28,9 +28,9 @@ def sign_login(client_id, **args):
   用户登录的签名
   """
   timestamp = int(time.time() * 1000)
-  none = secrets.token_hex(6)
-  text = f'{APP_ID}:{client_id}::{timestamp}:{none}'
-  data = get_signature(text, timestamp, none)
+  nonce = secrets.token_hex(6)
+  text = f'{APP_ID}:{client_id}::{timestamp}:{nonce}'
+  data = get_signature(text, timestamp, nonce)
   return data
 
 
@@ -40,10 +40,10 @@ def sign_chat(client_id, member_ids, **args):
   对话签名
   """
   timestamp = int(time.time() * 1000)
-  none = secrets.token_hex(6)
+  nonce = secrets.token_hex(6)
   member_ids.sort()
-  text = f'{APP_ID}:{client_id}:{":".join(member_ids)}:{timestamp}:{none}'
-  data = get_signature(text, timestamp, none)
+  text = f'{APP_ID}:{client_id}:{":".join(member_ids)}:{timestamp}:{nonce}'
+  data = get_signature(text, timestamp, nonce)
   return data
 
 
@@ -53,10 +53,10 @@ def sign_group(client_id, conv_id, member_ids, action, **args):
   群组功能的签名, action in ['invite', 'kick']
   """
   timestamp = int(time.time() * 1000)
-  none = secrets.token_hex(6)
+  nonce = secrets.token_hex(6)
   member_ids.sort()
-  text = f'{APP_ID}:{client_id}:{conv_id}:{":".join(member_ids)}:{timestamp}:{none}:{action}'
-  data = get_signature(text, timestamp, none)
+  text = f'{APP_ID}:{client_id}:{conv_id}:{":".join(member_ids)}:{timestamp}:{nonce}:{action}'
+  data = get_signature(text, timestamp, nonce)
   return data
 
 
@@ -65,13 +65,13 @@ def sign_chat_history(client_id, conv_id):
   """
   查询聊天记录的签名
   """
-  none = secrets.token_hex(6)
+  nonce = secrets.token_hex(6)
   signature_ts = int(time.time())
-  none = secrets.token_hex(6)
-  text = f'{APP_ID}:{client_id}:{conv_id}:{none}:{signature_ts}'
+  nonce = secrets.token_hex(6)
+  text = f'{APP_ID}:{client_id}:{conv_id}:{nonce}:{signature_ts}'
 
   signature = hmac.new(MASTER_KEY.encode('utf-8'), text.encode('utf-8'), hashlib.sha1).hexdigest()
-  data = {'signature': signature, 'signature_ts': signature_ts, 'none': none}
+  data = {'signature': signature, 'signature_ts': signature_ts, 'nonce': nonce}
   return data
 
 
@@ -81,12 +81,12 @@ def sign_block(client_id, conv_id, action, member_ids=None, **args):
   黑名单的签名
   """
   timestamp = int(time.time() * 1000)
-  none = secrets.token_hex(6)
+  nonce = secrets.token_hex(6)
   if member_ids:
     member_ids.sort()
-    text = f'{APP_ID}:{client_id}:{conv_id}:{":".join(member_ids)}:{timestamp}:{none}:{action}'
+    text = f'{APP_ID}:{client_id}:{conv_id}:{":".join(member_ids)}:{timestamp}:{nonce}:{action}'
   else:
-    text = f'{APP_ID}:{client_id}:{conv_id}::{timestamp}:{none}:{action}'
+    text = f'{APP_ID}:{client_id}:{conv_id}::{timestamp}:{nonce}:{action}'
 
-  data = get_signature(text, timestamp, none)
+  data = get_signature(text, timestamp, nonce)
   return data
